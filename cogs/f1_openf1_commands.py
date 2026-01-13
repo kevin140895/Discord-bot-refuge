51961e3d73e25acfc400d51baa6d9ceacab4e514
kevin140895 <Kevin.devassinea1408@gmail.com>
2026-01-13 17:37:41 +0100

    Add setup for f1 openf1 cog

--- cogs/f1_openf1_commands.py
+++ cogs/f1_openf1_commands.py
@@ -1,1 +1,10 @@
+from discord.ext import commands
 
+
+class F1OpenF1Commands(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(F1OpenF1Commands(bot))
