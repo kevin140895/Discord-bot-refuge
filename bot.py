5f770f9717b9e5b61a36b150986e6ea657f09420
kevin140895 <Kevin.devassinea1408@gmail.com>
2026-08-13 01:46:04 +0200

    fix: close background tasks with bot

--- bot.py
+++ bot.py
@@ -21,6 +21,7 @@ import cogs
 from storage.xp_store import xp_store
 from ui.radio_view import RadioView
 from utils.api_meter import api_meter
+from utils.background_tasks import background_tasks
 from utils.discord_api_trace import create_discord_http_trace
 from utils.rename_manager import rename_manager
 from utils.rate_limit import GlobalRateLimiter, limiter as _limiter
@@ -185,6 +186,9 @@ class RefugeBot(commands.Bot):
         await limiter.aclose()
         await api_meter.aclose()
         await rename_manager.aclose()
+        # Stop generic fire-and-forget work before closing storage so no
+        # checkpoint can outlive the resources it persists.
+        await background_tasks.aclose()
         await xp_store.aclose()
         await super().close()
 
@@ -214,6 +218,7 @@ __all__ = [
     "xp_store",
     "rename_manager",
     "api_meter",
+    "background_tasks",
     "limiter",
     "reset_http_error_counter",
     "create_bot",
